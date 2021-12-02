19666affe39e8f156bfb02ffb00f83e4f2f28ce8
Luis Medel <luis@medel.email>
2021-12-02 07:21:32 +0100

    Problema 2, parte 2

---
+++ 02_02/index.js
@@ -0,0 +1,31 @@
+
+import * as fs from 'fs';
+
+try
+{
+	let text = fs.readFileSync ("input.txt", "utf8"),
+		commands = text.split ("\n").map (t => t.split (" ", 2));
+
+	let x = 0,
+		y = 0,
+		aim = 0;
+
+	commands.forEach (function (cmd) {
+		
+		let amount = parseInt (cmd[1], 10);
+		
+		switch (cmd[0])
+		{
+			case "forward": { x += amount; y += aim * amount; break };
+			case "up": aim -= amount; break;
+			case "down": aim += amount; break;
+			default: throw `Unsupported command ${cmd[0]}`;
+		}
+	});
+	
+	console.log(x * y);
+}
+catch (err)
+{
+	console.error (err);
+}
